02c9e9ca3625331a7598171b9c7e6e03e81332db
Darkbreath89 <Darkbreath89@mail.com>
2017-01-05 19:10:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,8 @@ def inventory():
 			elif request.form['radio']=="3":
 				if form.validate_on_submit():
 					list+=Package.query.filter_by(id=request.form['radioform'])
+			if len(list)<1 and len(form.radioform.errors)<1:
+				form.radioform.errors.append("No entry")		
 		else:
 			for f in request.form.getlist('row'):
 				Package.query.filter_by(id=f).delete()
